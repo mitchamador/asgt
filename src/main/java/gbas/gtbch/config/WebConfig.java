8885f84334af55592661873f77818aa2b26e0cce
Viktar Pakhomau <victor@ktc.rw>
2021-12-13 15:51:22 +0300

    Add CORS exposed headers.

--- src/main/java/gbas/gtbch/config/WebConfig.java
+++ src/main/java/gbas/gtbch/config/WebConfig.java
@@ -3,6 +3,7 @@ package gbas.gtbch.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .exposedHeaders(HttpHeaders.AUTHORIZATION)
+        ;
     }
 }
