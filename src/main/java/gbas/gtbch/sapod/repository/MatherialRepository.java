174f8988622d51f333b7ac9b540d474cb4643c30
Viktar Pakhomau <victor@ktc.rw>
2021-12-17 09:42:20 +0300

    Fix small error.

--- src/main/java/gbas/gtbch/sapod/repository/MatherialRepository.java
+++ src/main/java/gbas/gtbch/sapod/repository/MatherialRepository.java
@@ -41,13 +41,16 @@ public class MatherialRepository {
 
     private final MatherialNdsRepository matherialNdsRepository;
 
+    private final MeasureRepository measureRepository;
+
     @Autowired
-    public MatherialRepository(@Qualifier("sapodDataSource") DataSource dataSource, DbHelper dbHelper, MatherialKoefRepository matherialKoefRepository, MatherialPriceRepository matherialPriceRepository, MatherialNdsRepository matherialNdsRepository) {
+    public MatherialRepository(@Qualifier("sapodDataSource") DataSource dataSource, DbHelper dbHelper, MatherialKoefRepository matherialKoefRepository, MatherialPriceRepository matherialPriceRepository, MatherialNdsRepository matherialNdsRepository, MeasureRepository measureRepository) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
         this.dbHelper = dbHelper;
         this.matherialKoefRepository = matherialKoefRepository;
         this.matherialPriceRepository = matherialPriceRepository;
         this.matherialNdsRepository = matherialNdsRepository;
+        this.measureRepository = measureRepository;
     }
 
     /**
@@ -85,21 +88,21 @@ public class MatherialRepository {
                 "left outer join measure mea1 on mea1.measure = m.measure\n" +
                 "left outer join measure mea2 on mea2.measure = m.measure_right\n";
 
+        if (!all) {
+            sql += "where (m.is_delete != 1 or m.is_delete is null)\n";
+        }
+
 //        if (dateBegin != null && dateEnd != null) {
-//            sql += "(date_b >= ? AND date_e <= ?) AND\n";
+//            sql += "and (date_b >= ? AND date_e <= ?)\n";
 //            args.add(dateBegin);
 //            args.add(dateEnd);
 //        } else if (dateBegin != null) {
-//            sql += "date_b >= ? AND\n";
+//            sql += "and date_b >= ?\n";
 //            args.add(dateBegin);
 //        } else if (dateEnd != null) {
-//            sql += "date_e <= ? AND\n";
+//            sql += "and date_e <= ?\n";
 //            args.add(dateEnd);
 //        }
-
-        if (!all) {
-            sql += "and m.is_delete != 1\n";
-        }
 
         sql += "order by code_ekisufr, code";
 
@@ -206,17 +209,17 @@ public class MatherialRepository {
                 preparedStatement = connection.prepareStatement("insert into matherial (\n" +
                                 " code, code_ekisufr, name, descriptor, measure,\n" +
                                 " measure_name_left, measure_right, measure_name_right, code_group, osob_name,\n" +
-                                " osob_val)\n" +
+                                " osob_val, is_delete)\n" +
                                 " values\n" +
                                 " (?, ?, ?, ?, ?,\n" +
                                 "  ?, ?, ?, ?, ?,\n" +
-                                "  ?)",
+                                "  ?, 0)",
                         Statement.RETURN_GENERATED_KEYS);
             } else {
                 preparedStatement = connection.prepareStatement("update matherial set\n" +
                         " code=?, code_ekisufr=?, name=?, descriptor=?, measure=?,\n" +
                         " measure_name_left=?, measure_right=?, measure_name_right=?, code_group=?, osob_name=?,\n" +
-                        " osob_val=?\n" +
+                        " osob_val=?, is_delete=0\n" +
                         " where matherial = ?");
             }
 
@@ -226,14 +229,14 @@ public class MatherialRepository {
             preparedStatement.setInt(4, matherial.getDescriptor());
             if (matherial.getMeasureLeft() != null) {
                 preparedStatement.setInt(5, matherial.getMeasureLeft().getId());
-                preparedStatement.setString(6, matherial.getMeasureLeft().getName());
+                preparedStatement.setString(6, getMeasureName(matherial.getMeasureLeft()));
             } else {
                 preparedStatement.setNull(5, Types.INTEGER);
                 preparedStatement.setNull(6, Types.VARCHAR);
             }
             if (matherial.getMeasureRight() != null) {
                 preparedStatement.setInt(7, matherial.getMeasureRight().getId());
-                preparedStatement.setString(8, matherial.getMeasureRight().getName());
+                preparedStatement.setString(8, getMeasureName(matherial.getMeasureRight()));
             } else {
                 preparedStatement.setNull(7, Types.INTEGER);
                 preparedStatement.setNull(8, Types.VARCHAR);
@@ -260,6 +263,43 @@ public class MatherialRepository {
         }
 
         return matherial.getId();
+    }
+
+    private List<Measure> measureList;
+
+    /**
+     * get {@link Measure}
+     * @param id
+     * @return
+     */
+    private Measure getMeasure(int id) {
+        if (measureList == null) {
+            measureList = measureRepository.getMeasures();
+        }
+        if (measureList != null) {
+            return measureList.stream().filter(m -> m.getId() == id).findFirst().orElse(null);
+        }
+        return null;
+    }
+
+    /**
+     * get {@link Measure} name
+     * @param m
+     * @return
+     */
+    private String getMeasureName(Measure m) {
+        String measureName = null;
+        if (m != null) {
+            if (m.getName() != null) {
+                measureName = m.getName();
+            } else {
+                Measure measure = getMeasure(m.getId());
+                if (measure != null) {
+                    return measure.getName();
+                }
+            }
+        }
+        return measureName;
     }
 
     /**
