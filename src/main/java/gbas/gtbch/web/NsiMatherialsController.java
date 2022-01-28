152e7dae6d47a161dd5ee11d5cfde3b0c1c025ef
Viktar Pakhomau <victor@ktc.rw>
2022-01-28 08:58:06 +0300

    Add matherials editor.

--- src/main/java/gbas/gtbch/web/NsiMatherialsController.java
+++ src/main/java/gbas/gtbch/web/NsiMatherialsController.java
@@ -1,5 +1,10 @@
 package gbas.gtbch.web;
 
+import gbas.gtbch.sapod.model.matherials.Matherial;
+import gbas.gtbch.sapod.model.matherials.Measure;
+import gbas.gtbch.sapod.model.matherials.SborDescriptorItem;
+import gbas.gtbch.sapod.service.MatherialService;
+import gbas.gtbch.sapod.service.MeasureService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,11 +14,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
 @Controller
 @RequestMapping("/user/nsi/matherials")
 public class NsiMatherialsController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final MatherialService matherialService;
+    private final MeasureService measureService;
+
+    public NsiMatherialsController(MatherialService matherialService, MeasureService measureService) {
+        this.matherialService = matherialService;
+        this.measureService = measureService;
+    }
 
     /**
      * matherials page
@@ -30,9 +48,38 @@ public class NsiMatherialsController {
      * get {@link gbas.gtbch.sapod.model.matherials.Matherial}
      * @param id - matherial.matherial
      */
-    @RequestMapping(value = "/matherials/{id:[\\d]+}/editor", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id:[\\d]+}/editor", method = RequestMethod.GET)
     public ModelAndView getDocumentEditor(@PathVariable int id) {
         ModelAndView model = new ModelAndView("fragments/matherials :: matherialEditor");
+
+        Matherial matherial;
+        if (id == 0) {
+            matherial = new Matherial();
+
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.MONTH, 0);
+            c.set(Calendar.DAY_OF_MONTH, 1);
+            c.set(Calendar.HOUR_OF_DAY, 0);
+            c.clear(Calendar.MINUTE);
+            c.clear(Calendar.SECOND);
+            c.clear(Calendar.MILLISECOND);
+            matherial.setDateBegin(c.getTime());
+
+            c.add(Calendar.YEAR, 1);
+            c.add(Calendar.DAY_OF_YEAR, -1);
+            matherial.setDateEnd(c.getTime());
+
+        } else {
+            matherial = matherialService.getMatherial(id);
+        }
+
+        model.addObject("matherial", matherial);
+
+        model.addObject("descriptors", SborDescriptorItem.getList());
+
+        List<Measure> measures = new ArrayList<>(Collections.singletonList(new Measure(0, "")));
+        measures.addAll(measureService.getMeasureList());
+        model.addObject("measures", measures);
 
         return model;
     }
--- src/test/java/gbas/gtbch/sapod/repository/TPolRepositoriesTest.java
+++ src/test/java/gbas/gtbch/sapod/repository/TPolRepositoriesTest.java
@@ -1,5 +1,7 @@
 package gbas.gtbch.sapod.repository;
 
+import gbas.gtbch.sapod.model.tpol.TpClient;
+import gbas.gtbch.sapod.model.tpol.TpLinkedClient;
 import gbas.gtbch.sapod.model.tpol.TpTvkKof;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -34,5 +37,61 @@ public class TPolRepositoriesTest {
                 tPolKofRepository.deleteKof(id);
             }
         }
+    }
+
+    @Autowired
+    TPolRepository tPolRepository;
+
+    @Test
+    public void testTpClient() {
+
+        List<TpClient> tpClients = tPolRepository.getTpClients();
+        if (!tpClients.isEmpty()) {
+            System.out.println(tpClients.get(0).toString());
+        }
+
+
+        {
+            List<TpLinkedClient> tpLinkedClients = tPolRepository.getLinkedTpClients(18581);
+            tpLinkedClients.forEach(tpLinkedClient -> System.out.println(tpLinkedClient.toString()));
+        }
+
+        {
+            List<TpLinkedClient> tpLinkedClients = new ArrayList<>();
+            for (int i = 0; i < 2; i++) {
+                TpLinkedClient tpLinkedClient = new TpLinkedClient();
+                tpLinkedClient.setCode(String.valueOf(100500 + i));
+                tpLinkedClient.setName("test" + (i + 1));
+                tpLinkedClient.setNumNod(i + 4);
+                tpLinkedClients.add(tpLinkedClient);
+            }
+            tPolRepository.saveLinkedTpClients(18486, tpLinkedClients);
+
+            tpLinkedClients = tPolRepository.getLinkedTpClients(18486);
+            tpLinkedClients.forEach(tpLinkedClient -> System.out.println(tpLinkedClient.toString()));
+        }
+
+        {
+            List<TpLinkedClient> tpLinkedClients = new ArrayList<>();
+            for (int i = 5; i < 7; i++) {
+                TpLinkedClient tpLinkedClient = new TpLinkedClient();
+                tpLinkedClient.setCode(String.valueOf(100500 + i));
+                tpLinkedClient.setName("test" + (i + 1));
+                tpLinkedClient.setNumNod(i);
+                tpLinkedClients.add(tpLinkedClient);
+            }
+
+            tPolRepository.saveLinkedTpClients(18486, tpLinkedClients);
+
+            tpLinkedClients = tPolRepository.getLinkedTpClients(18486);
+            tpLinkedClients.forEach(tpLinkedClient -> System.out.println(tpLinkedClient.toString()));
+        }
+
+        {
+            tPolRepository.deleteLinkedTpClients(18486);
+            List<TpLinkedClient> tpLinkedClients = tPolRepository.getLinkedTpClients(18486);
+            tpLinkedClients.forEach(tpLinkedClient -> System.out.println(tpLinkedClient.toString()));
+        }
+
     }
 }
